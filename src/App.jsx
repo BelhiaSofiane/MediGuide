40bdfe08ad25e0b66abc2f2467934cf180612646
sofiane belhia <sofianebelhia150@gmail.com>
2025-08-04 11:13:51 +0100

    -add SymptomChecker page with metadata for now need to implement ai next

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import SymptomChecker from './pages/SymptomChecker';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 
@@ -8,6 +9,7 @@ function App() {
 
   return (
           <Routes>
+            <Route path="/SymptomChecker" element={<SymptomChecker />} />
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
           </Routes>
